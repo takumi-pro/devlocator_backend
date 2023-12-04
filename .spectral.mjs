c0eb0b8f13d44339e7a9f3103fce00889e59ee1a
takumidiary.0927@gmail.com <takumidiary.0927@gmail.com>
2023-12-04 22:02:55 +0900

    feature: ホスティング用のサンプルAPI仕様書作成

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyODM0MQ/spectral.js?branch_slug=main&token=88f2b7da-e9b4-4623-afc6-d3e4305583a4";
+export default { extends: ruleset };
