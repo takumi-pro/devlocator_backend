72f19e8315130f98f805ccb50d62dd66345fe9c6
takumi <takumidiary.0927@gmail.com>
2024-02-09 15:24:24 +0900

    feature: migration

--- migrations/20231218194631-init.sql
+++ migrations/20231218194631-init.sql
@@ -1,14 +1,53 @@
 
 -- +migrate Up
 CREATE TABLE `users` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL COMMENT 'ユーザ名',
-  `email` varchar(255) NOT NULL COMMENT 'メールアドレス',
-  `firebase_uid` varchar(255) NOT NULL COMMENT 'Firebaseが持っているID',
-  `image` varchar(255) NOT NULL COMMENT '表示用写真URL',
+  `id` VARCHAR(255) NOT NULL COMMENT 'ユーザ識別子',
+  `name` VARCHAR(255) COMMENT 'ユーザ名',
+  `email` VARCHAR(255) NOT NULL COMMENT 'メールアドレス',
+  `emailVerified` DATETIME(6) COMMENT 'メールアドレスの認証日時',
+  `image` VARCHAR(255) COMMENT '表示用写真URL',
   `created_at` timestamp,
-  `updated_at` timestamp
-);
+  `updated_at` timestamp,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_email` (`email`)
+) COMMENT 'Auth.jsでユーザの識別子を管理するテーブル';
+
+CREATE TABLE `accounts` (
+  `userId` varchar(255) NOT NULL COMMENT 'userテーブルのid、ユーザーの識別子',
+  `type` varchar(255) NOT NULL COMMENT 'oauth等の値が入る',
+  `provider` varchar(255) NOT NULL COMMENT 'google, line等の認証Providerの名称が入る',
+  `providerAccountId` varchar(255) NOT NULL COMMENT 'google, line等の認証Providerのユーザー識別子が入る',
+  `refresh_token` text COMMENT '認証Providerから取得したリフレッシュトークンが記録される',
+  `access_token` text COMMENT '認証Providerから取得したアクセストークンが記録される',
+  `expires_at` int DEFAULT NULL COMMENT '認証Providerから取得したアクセストークンの有効期限',
+  `token_type` varchar(255) DEFAULT NULL COMMENT 'Bearer等のトークン種別が記録される',
+  `scope` varchar(255) DEFAULT NULL COMMENT 'リクエスト時に認証Providerに要求したscopeが記録される',
+  `id_token` text COMMENT '認証Providerから取得したIDトークンの有効期限',
+  `session_state` varchar(255) DEFAULT NULL,
+  `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`provider`,`providerAccountId`),
+  UNIQUE KEY `uk_account_userid` (`userId`),
+  KEY `idx_account_01` (`userId`)
+) COMMENT 'Auth.jsでユーザの認証情報を管理するテーブル';
+
+CREATE TABLE `sessions` (
+  `sessionToken` varchar(255) NOT NULL COMMENT 'セッションの識別子が格納される',
+  `userId` varchar(255) NOT NULL COMMENT 'userテーブルのid、ユーザーの識別子',
+  `expires` timestamp NOT NULL COMMENT 'セッションの有効期限が格納される',
+  `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`sessionToken`),
+  UNIQUE KEY `uk_session_userid` (`userId`),
+  KEY `idx_session_01` (`userId`)
+) COMMENT 'Auth.jsでユーザーのセッションを管理するテーブル session.strategyがdatabaseの時だけ利用される';
+
+CREATE TABLE `verificationtokens` (
+  `identifier` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expires` timestamp NOT NULL,
+  PRIMARY KEY (`identifier`,`token`)
+) COMMENT 'Auth.jsでメールアドレスを使ったパスワードレスログインを利用する時に利用される';
 
 CREATE TABLE `events` (
   `event_id` INT PRIMARY KEY NOT NULL COMMENT 'イベントID',
@@ -31,7 +70,7 @@ CREATE TABLE `events` (
   `address` VARCHAR(255) COMMENT '開催住所',
   `lat` VARCHAR(255) NOT NULL COMMENT '緯度',
   `lon` VARCHAR(255) NOT NULL COMMENT '経度'
-);
+) COMMENT 'イベント情報を管理するテーブル';
 
 CREATE TABLE `bookmarked_events` (
   `event_id` INT PRIMARY KEY NOT NULL COMMENT 'イベントID',
@@ -54,17 +93,20 @@ CREATE TABLE `bookmarked_events` (
   `address` VARCHAR(255) COMMENT '開催住所',
   `lat` VARCHAR(255) NOT NULL COMMENT '緯度',
   `lon` VARCHAR(255) NOT NULL COMMENT '経度'
-);
+) COMMENT 'ブックマークされたイベントを管理するテーブル';
 
 CREATE TABLE `bookmarks` (
   `event_id` INT COMMENT 'イベントID',
   `user_id` INT COMMENT 'ユーザID',
   PRIMARY KEY (`event_id`, `user_id`)
-);
+) COMMENT 'ユーザとユーザがブックマークしたイベントを紐づけるテーブル';
 
 
 -- +migrate Down
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS verificationtokens;
 DROP TABLE IF EXISTS events;
 DROP TABLE IF EXISTS bookmarked_events;
 DROP TABLE IF EXISTS bookmarks;
